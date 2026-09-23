02bb1f7c68e2accb9f71fca4ad874fd298ccff70
xxleeks-png <xxleeks@gmail.com>
2026-09-23 18:06:29 +0800

    feat: route solving through SolverRegistry

--- math_engine/solver.py
+++ math_engine/solver.py
@@ -1,6 +1,9 @@
 from fractions import Fraction
 from .models import MathSolution
 from .problem import ProblemSpec
+from .registry import registry
+from .solvers_basic import solve_arithmetic, solve_fraction, solve_rectangle
+from . import validator
 
 
 def solve_water_tank_spec(spec: ProblemSpec) -> MathSolution:
@@ -8,37 +11,17 @@ def solve_water_tank_spec(spec: ProblemSpec) -> MathSolution:
     outlet = Fraction(1, int(spec.known["outlet_hours"]))
     both = Fraction(1, int(spec.known["rainy_both_hours"]))
     rain = both + outlet - inlet
-    effective_outflow = outlet - rain
-    answer_time = Fraction(1, 1) / effective_outflow
+    effective = outlet - rain
+    answer_time = Fraction(1, 1) / effective
+    return MathSolution(problem=spec.raw_text, known_conditions=["进水速度=1/6", "出水速度=1/8", "雨天净速度=1/12"], goal="求雨天只开出水管时的排水时间", steps=["把满池水量看作1", f"雨水速度=1/12+1/8-1/6={rain}", f"实际排水速度=1/8-{rain}={effective}", f"排完一池水需要=1÷{effective}={answer_time}小时"], answer=f"{answer_time}小时", verified=False, knowledge_point="工程问题 / 流水问题")
 
-    return MathSolution(
-        problem=spec.raw_text,
-        known_conditions=[
-            f"进水管单独{int(spec.known['inlet_hours'])}小时装满，所以进水速度为1/{int(spec.known['inlet_hours'])}池/小时",
-            f"出水管单独{int(spec.known['outlet_hours'])}小时排空，所以出水速度为1/{int(spec.known['outlet_hours'])}池/小时",
-            f"雨天两管同时开，{int(spec.known['rainy_both_hours'])}小时注满，所以净增加速度为1/{int(spec.known['rainy_both_hours'])}池/小时",
-        ],
-        goal="求雨天只开出水管时把满池水排完需要多少小时",
-        steps=[
-            "把满池水量看作1",
-            f"进水管速度：1÷{int(spec.known['inlet_hours'])}=1/{int(spec.known['inlet_hours'])}",
-            f"出水管速度：1÷{int(spec.known['outlet_hours'])}=1/{int(spec.known['outlet_hours'])}",
-            f"雨天两管同时开，水池每小时净增加1÷{int(spec.known['rainy_both_hours'])}=1/{int(spec.known['rainy_both_hours'])}",
-            f"雨水速度：1/{int(spec.known['rainy_both_hours'])}+1/{int(spec.known['outlet_hours'])}-1/{int(spec.known['inlet_hours'])}={rain}",
-            f"雨天只开出水管时，实际排水速度：1/{int(spec.known['outlet_hours'])}-{rain}={effective_outflow}",
-            f"排完一池水需要：1÷{effective_outflow}={answer_time}小时",
-        ],
-        answer=f"{answer_time}小时",
-        verified=False,
-        knowledge_point="工程问题 / 流水问题",
-    )
+
+registry.register("流水问题", solve_water_tank_spec)
 
 
 def solve(problem: str) -> MathSolution:
     from .parser import parse
-    from .validator import validate
-
     spec = parse(problem)
-    solution = solve_water_tank_spec(spec)
-    solution.verified = validate(spec, solution)
+    solution = registry.get(spec.sub_type)(spec)
+    solution.verified = validator.validate(spec, solution)
     return solution
