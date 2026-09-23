cde0baf45ea1022ec8e7f0f02517cf0206e769a3
xxleeks-png <xxleeks@gmail.com>
2026-09-23 22:22:20 +0800

    add unicode-safe sample launcher

---
+++ scripts/run_sample.py
@@ -0,0 +1,9 @@
+from generate_video import generate_video
+
+PROBLEM = "一个空水池，单独开进水管，6小时可以注满；单独开出水管，8小时可以放完一池水。现在雨天雨水匀速注入池中，同时打开进水管和出水管，12小时刚好注满水池。如果雨天只开出水管，多少小时可以把满池水放完？"
+
+if __name__ == "__main__":
+    result = generate_video(PROBLEM, "output/sample_01")
+    print("")
+    print("SAMPLE VIDEO GENERATED")
+    print(result["video_path"])
